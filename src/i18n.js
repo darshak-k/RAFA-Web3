c1dc8dfc4c905ab8351d4899ade62a554c468caf
gauravmalaviya879 <malaviyagaurav879@gmail.com>
2024-03-18 00:21:04 +0530

    rafa setup

---
+++ src/i18n.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import enTranslation from './assets/locales/en/translation.json';
+import hiTranslation from './assets/locales/hi/translation.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslation },
+      hi: { translation: hiTranslation }
+    },
+    lng: 'en', // default language
+    fallbackLng: 'en', // fallback language
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+export default i18n;
