dba92ac031f0189acb070fa05c95777fde9da788
gauravmalaviya879 <malaviyagaurav879@gmail.com>
2024-03-21 14:16:59 +0530

    change this new componets

--- src/assets/componets/ExchangeCoin.jsx
+++ src/assets/componets/ExchangeCoin.jsx
@@ -1,37 +1,51 @@
 import React from "react";
-import cryptoLogo from "../img/cryptoLogo.png";
-import logoOne from "../img/logoOne.png";
-import logoTwo from "../img/logoTwo.png";
-import logoThree from "../img/logoThree.png";
+import cryptoLogo from "../img/logo-crypto.png";
+import cryptoTwo from "../img/crypto-two.png";
 
 
 const ExchangeCoin = () => {
   return (
     <section id="exchangeCoin" className="bg-theam-second py-2">
-      <div className="container">
-        <div className="w-80 pt-5">
-          <h2 className=" p-0  w-60 font-monst-reg font-30 m-0">
-            Easy to buy, easy to use:  Buy and trade the $RAFA token on the
-          </h2>
-          <h2 className="font-monst-bold p-0  font-34 m-0 text-them-black" >world’s top exchanges very soon</h2>
-       
-          <div className="d-flex ">
-            <img src={cryptoLogo} className="img-fluid img-crypto-exchange"/>
-        </div>
-        <div className="d-flex justify-content-between">
-             <div className="center-h">
-                <img className="img-fluid" src={logoOne}></img>
-             </div>
-             <div className="center-h">
-                <img className="img-fluid" src={logoTwo}></img>
-             </div>
-             
-             <div className="center-h">
-                <img className="img-fluid" src={logoThree}></img>
-             </div>
-        </div>
-        </div>
+     
+      <div className="container-fluid">
+        <div className="row ">
+          <div className="col-md-6 py-5 col-12">
+            <div className="d-flex h-100 flex-column justify-content-between">
+              <h2 className=" p-0  w-80 font-monst-reg font-30 m-0">
+                Easy to buy, easy to use:  Buy and trade the $RAFA token on the
+              </h2>
+              <h2 className="font-monst-bold p-0  font-34 m-0 text-them-black">
+                world’s top exchanges very soon
+              </h2>
+
+              <div className="d-flex my-5  w-80">
+                <img
+                  src={cryptoLogo}
+                  className="img-fluid"
+                />
+              </div>
+              <h1 className="text-center font-monst-bold font-34">Total Supply: 1billion</h1>
+            </div>
+          </div>
+          <div className="col-md-6 py-5 col-12">
       
+            <div className="d-flex h-100 flex-column justify-content-between">
+              <h2 className=" p-0  w-80 font-monst-reg font-30 m-0">
+              To buy $RAFA, get phantom wallet, Purchase $SOL and send it to your Phantom wallet.
+              </h2>
+              
+
+              <div className="d-flex my-5  w-80">
+                <img
+                  src={cryptoTwo}
+                  className="img-fluid"
+                />
+              </div>
+              <h1 className="text-center font-monst-bold font-34">tokens locked for 2 yers: 300 million </h1>
+            </div>
+         
+          </div>
+        </div>
       </div>
     </section>
   );
