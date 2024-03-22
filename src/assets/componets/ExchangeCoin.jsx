475330e9a6e5c1dae66c9a5012d524164787f7ad
gauravmalaviya879 <malaviyagaurav879@gmail.com>
2024-03-22 08:27:52 +0530

    "change componets exchange coin padding add"

--- src/assets/componets/ExchangeCoin.jsx
+++ src/assets/componets/ExchangeCoin.jsx
@@ -7,14 +7,14 @@ const ExchangeCoin = () => {
   return (
     <section id="exchangeCoin" className="bg-theam-second py-2">
      
-      <div className="container-fluid">
+      <div className="container">
         <div className="row ">
           <div className="col-md-6 py-5 col-12">
             <div className="d-flex h-100 flex-column justify-content-between">
               <h2 className=" p-0  w-80 font-monst-reg font-30 m-0">
                 Easy to buy, easy to use:  Buy and trade the $RAFA token on the
               </h2>
-              <h2 className="font-monst-bold p-0  font-34 m-0 text-them-black">
+              <h2 className="font-monst-bold p-0  font-30 m-0 text-them-black">
                 world’s top exchanges very soon
               </h2>
 
@@ -24,7 +24,7 @@ const ExchangeCoin = () => {
                   className="img-fluid"
                 />
               </div>
-              <h1 className="text-center font-monst-bold font-34">Total Supply: 1billion</h1>
+              <h1 className="text-center font-monst-bold font-30">Total Supply: 1billion</h1>
             </div>
           </div>
           <div className="col-md-6 py-5 col-12">
@@ -41,7 +41,7 @@ const ExchangeCoin = () => {
                   className="img-fluid"
                 />
               </div>
-              <h1 className="text-center font-monst-bold font-34">tokens locked for 2 yers: 300 million </h1>
+              <h1 className="text-center font-monst-bold font-30">tokens locked for 2 yers: 300 million </h1>
             </div>
          
           </div>
